2061f91a4fa24fe7bb807f8342568ced1ced7321
Gael Mousset <gaelmousset@orange.fr>
2024-07-28 07:53:30 +0200

    fix appel api

--- server.js
+++ server.js
@@ -4,6 +4,7 @@ const express = require("express");
 const mongoose = require("mongoose");
 const cors = require("cors");
 const bodyParser = require("body-parser");
+const axios = require("axios");
 
 const app = express();
 const port = process.env.PORT || 5000;
@@ -22,6 +23,21 @@ mongoose
 // Routes
 const games = require("./routes/games");
 app.use("/api/games", games);
+
+app.get("/proxy", async (req, res) => {
+  const name = req.query.name;
+  const apiKey =
+    "e2632452c70b87623a7abd8f06273b80f723db6158242bebb380284efa6a251c"; // Ideally, use process.env for keys
+  const url = `https://api.thegamesdb.net/v1/Games/ByGameName?apikey=${apiKey}&name=${name}`;
+
+  try {
+    const response = await axios.get(url);
+    res.send(response.data);
+  } catch (err) {
+    console.error("Error in /proxy route:", err.message);
+    res.status(500).send("Internal Server Error");
+  }
+});
 
 app.listen(port, () => {
   console.log(`Server running on port ${port}`);
